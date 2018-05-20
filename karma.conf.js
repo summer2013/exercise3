0144a745b6880bc4a114ad6e6e7208fcd3cc118a
shiny <shiny_mi@163.com>
2018-05-20 18:39:07 +0800

    add ci configuration

--- karma.conf.js
+++ karma.conf.js
@@ -58,7 +58,7 @@ module.exports = function(config) {
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     // browsers: ['Firefox', 'Safari', 'Chrome', 'IE'],
-    browsers: ['Chrome', 'Safari'],
+    browsers: ['Chrome'],
 
 	customLaunchers: {
 		ChromeHeadlessNoSandbox: {
